c4cf8cef6d2fee759d92b95badd0cccbe1920b0f
Rita <ritasharipova10@gmail.com>
2025-08-08 18:02:18 -0400

    cors updates adn timeout

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const { Resend } = require('resend');
 require("dotenv").config();
 
 const app = express();
-app.use(cors({origin: "https://rita-sharipova-portfolio.up.railway.app",
-methods: ["GET", "POST", "OPTIONS"]}));
+app.use(cors());
 
 app.use(express.json());
 app.options("/send", cors()); 
@@ -36,6 +35,10 @@ app.get("/health", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Backend running on http://0.0.0.0:${PORT}`);
-});
+setTimeout(() => {
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Backend running on http://0.0.0.0:${PORT}`);
+  });
+}, 2000);
+
+console.log("PORT env variable:", process.env.PORT);
