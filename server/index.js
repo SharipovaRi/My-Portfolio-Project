a07db566cb457ee4d63cd28be40df2a1cddd93dd
Rita <ritasharipova10@gmail.com>
2025-08-08 17:49:29 -0400

    To listen from outside of the container

--- server/index.js
+++ server/index.js
@@ -35,6 +35,7 @@ app.get("/health", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend running on http://0.0.0.0:${PORT}`);
 });
