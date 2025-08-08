dbaabafa19a37b3d7b8b30a4abe087624346268c
Rita <ritasharipova10@gmail.com>
2025-08-08 00:11:20 -0400

    change port num

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.post("/send", async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
 });
