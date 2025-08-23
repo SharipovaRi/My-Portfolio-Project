a2657f7b9e16e56d04b0e4528bb3654b3f0fcd02
Rita <ritasharipova10@gmail.com>
2025-08-23 03:12:12 -0400

    Update footer syntax

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import {ChevronUp, CircleArrowUp } from "lucide-react";
 
 export const Footer = () => {
     return <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-center items-center gap-x-4"> 
-            <p className="text-sm text-muted-foreground "> &copy; {new Date().getFullYear()} Rita Sharipova Portfolio, All rights reserved.</p> 
+            <p className="text-sm text-muted-foreground "> &copy; {new Date().getFullYear()} Rita Sharipova Portfolio. All rights reserved.</p> 
             <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-secondary transition-colors ">
                 <ChevronUp size={20}/>
             </a>
